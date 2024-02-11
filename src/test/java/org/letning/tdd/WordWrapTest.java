a7a18b183281e9ea550dc02577d78f355dc0de80
Jacek programmer <jacek301@gmail.com>
2024-02-11 23:00:01 +0100

    WordWrap name change

--- src/test/java/org/letning/tdd/WordWrapTest.java
+++ src/test/java/org/letning/tdd/WordWrapTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class WardWrapTest {
+public class WordWrapTest {
 
     @Test
     void firstTest() {

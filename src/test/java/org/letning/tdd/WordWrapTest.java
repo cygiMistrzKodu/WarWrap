90c0da9788fe1ec1e25d484bd46c5776b6fba62a
Jacek programmer <jacek301@gmail.com>
2024-02-12 23:01:59 +0100

    make some refactor

--- src/test/java/org/letning/tdd/WordWrapTest.java
+++ src/test/java/org/letning/tdd/WordWrapTest.java
@@ -1,19 +1,24 @@
 package org.letning.tdd;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class WordWrapTest {
 
+    private void assertWrapped(String s, int width, String expected) {
+        assertEquals(expected, wrap(s, width));
+    }
+
     @Test
     public void testWrap() {
-        assertEquals("Wołam", wrap("Wołam",7));
-        assertEquals("Wołam,\nja",wrap("Wołam, ja",7));
+        assertWrapped("Wołam",7,"Wołam");
+        assertWrapped("Wołam, ja",7,"Wołam,\nja");
 
     }
 
     private String wrap(String s, int w) {
-        return s.replace(" ","\n");
+        return s.replace(" ", "\n");
     }
 }
